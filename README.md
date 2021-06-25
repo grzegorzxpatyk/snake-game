743dd9db53cd2437611e15060c9eb231b8480cbe
Grzegorz Patyk <grzegorz.p4tyk@gmail.com>
2021-06-25 20:59:00 +0200

    Added snake-head turning feature

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # snake-game
+
+Snake game inspired by the vintage nokia cellphone game and Ania Kubów's tutorial on youtube.
+
+Here's the link to the tutorial. Base of the game is made based on Ania's work.
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const squares = document.querySelectorAll(".grid div");
-    const scoreDisplay = document.querySelector("span");
-    const startBtn = document.querySelector(".start");
-    const restartBtn = document.querySelector(".restart");
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div');
+    const scoreDisplay = document.querySelector('.score .score-display');
+    const scoreDisplayGameOver = document.querySelector(
+        '.score-game-over .score-display'
+    );
+    const startBtn = document.querySelector('.start');
+    const restartBtn = document.querySelector('.restart');
 
     const width = 10;
     let currentIndex = 0; // so first div in our grid
@@ -15,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let intervalTime = 0;
     let interval = 0;
 
-    document.querySelector(".game").classList.toggle("blur");
+    document.querySelector('.game').classList.toggle('blur');
 
     // function to toggle display of gameover screen
 
@@ -26,42 +29,33 @@ document.addEventListener("DOMContentLoaded", () => {
     function gameOverScreen() {
         if (currentSnake !== null) {
             document
-                .querySelector(".game-over-screen")
-                .classList.toggle("visible");
-            document.querySelector(".game").classList.toggle("blur");
+                .querySelector('.game-over-screen')
+                .classList.toggle('visible');
+            document.querySelector('.game').classList.toggle('blur');
         }
-
-        // if (squares.forEach((square) => squares[square].classList.contains("snake"))) {
-        //     console.log("grid does not contain the .snake class");
-        // } else {
-        //     document
-        //         .querySelector(".game-over-screen")
-        //         .classList.toggle("visible");
-        //     document.querySelector(".game").classList.toggle("blur");
-        // }
     }
 
     // to start, and restart the game
     function startGame() {
         if (
             !document
-                .querySelector(".new-game-screen")
-                .classList.contains("hidden")
+                .querySelector('.new-game-screen')
+                .classList.contains('hidden')
         ) {
-            document.querySelector(".new-game-screen").classList.add("hidden");
-            document.querySelector(".game").classList.toggle("blur");
+            document.querySelector('.new-game-screen').classList.add('hidden');
+            document.querySelector('.game').classList.toggle('blur');
         } else {
             gameOverScreen();
         }
         // document.querySelector(".game").classList.toggle("blur");
 
         currentSnake.forEach(
-            (index) => squares[index].classList.remove("snake") //removes the body of a snake
+            (index) => squares[index].classList.remove('snake') //removes the body of a snake
         );
 
-        squares[currentSnake[0]].classList.remove("snake-head"); //removes the head
+        squares[currentSnake[0]].classList.remove('snake-head'); //removes the head
 
-        squares[appleIndex].classList.remove("apple");
+        squares[appleIndex].classList.remove('apple');
         clearInterval(interval);
         score = 0;
 
@@ -69,11 +63,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
         direction = 1;
         scoreDisplay.innerText = score;
+        scoreDisplayGameOver.innerText = score;
         intervalTime = 600;
         currentSnake = [2, 1, 0];
         currentIndex = 0;
-        currentSnake.forEach((index) => squares[index].classList.add("snake"));
-        squares[currentSnake[0]].classList.add("snake-head");
+        currentSnake.forEach((index) => squares[index].classList.add('snake'));
+        squares[currentSnake[0]].classList.add('snake-head');
         interval = setInterval(moveOutcomes, intervalTime);
     }
 
@@ -85,7 +80,7 @@ document.addEventListener("DOMContentLoaded", () => {
             (currentSnake[0] % width === width - 1 && direction === 1) || //if snake hits right wall
             (currentSnake[0] % width === 0 && direction === -1) || //if snakes hits left wall
             (currentSnake[0] - width < 0 && direction === -width) || // if snake hits the top
-            squares[currentSnake[0] + direction].classList.contains("snake") //if snake goes into itself
+            squares[currentSnake[0] + direction].classList.contains('snake') //if snake goes into itself
         ) {
             gameOverScreen();
             return clearInterval(interval); //this will clear the interval if any of the above happen
@@ -94,51 +89,81 @@ document.addEventListener("DOMContentLoaded", () => {
         //outcome of those ^^ is GAME OVER
 
         const tail = currentSnake.pop(); // removes last ite of the array and shows it
-        squares[tail].classList.remove("snake"); // removes class of snake from the TAIL
-        squares[currentSnake[0]].classList.remove("snake-head");
+        squares[tail].classList.remove('snake'); // removes class of snake from the TAIL
+        squares[currentSnake[0]].classList.remove('snake-head');
+        squares[currentSnake[0]].classList.remove('right');
+        squares[currentSnake[0]].classList.remove('left');
+        squares[currentSnake[0]].classList.remove('up');
+        squares[currentSnake[0]].classList.remove('down');
         currentSnake.unshift(currentSnake[0] + direction); // gives direction to the head of the array
 
         //deals with snake hitting APPLE
-        if (squares[currentSnake[0]].classList.contains("apple")) {
-            squares[currentSnake[0]].classList.remove("apple");
-            squares[tail].classList.add("snake");
+        if (squares[currentSnake[0]].classList.contains('apple')) {
+            squares[currentSnake[0]].classList.remove('apple');
+            squares[tail].classList.add('snake');
             currentSnake.push(tail);
             randomApple();
             score++;
             scoreDisplay.textContent = score;
+            scoreDisplayGameOver.textContent = score;
             clearInterval(interval);
             intervalTime = intervalTime * speed;
             interval = setInterval(moveOutcomes, intervalTime);
         }
 
-        squares[currentSnake[0]].classList.add("snake");
-        squares[currentSnake[0]].classList.add("snake-head");
+        squares[currentSnake[0]].classList.add('snake');
+        squares[currentSnake[0]].classList.add('snake-head');
+
+        if (direction === 1) {
+            squares[currentSnake[0]].classList.add('right');
+            
+        } else if (direction === -1) {
+            squares[currentSnake[0]].classList.remove('right');
+            squares[currentSnake[0]].classList.remove('up');
+            squares[currentSnake[0]].classList.remove('down');
+            
+            squares[currentSnake[0]].classList.add('left');
+        } else if (direction === -10) {
+            squares[currentSnake[0]].classList.remove('right');
+            squares[currentSnake[0]].classList.remove('left');
+            squares[currentSnake[0]].classList.remove('down');
+            
+            squares[currentSnake[0]].classList.add('up');
+        } else if (direction === 10) {
+            squares[currentSnake[0]].classList.remove('right');
+            squares[currentSnake[0]].classList.remove('up');
+            squares[currentSnake[0]].classList.remove('left');
+            
+            squares[currentSnake[0]].classList.add('down');
+        } 
     }
 
     //generate new apple once apple is eaten
     function randomApple() {
         do {
             appleIndex = Math.floor(Math.random() * squares.length);
-        } while (squares[appleIndex].classList.contains("snake")); //making sure that apple doesnt appear "on snake"
-        squares[appleIndex].classList.add("apple");
+        } while (squares[appleIndex].classList.contains('snake')); //making sure that apple doesnt appear "on snake"
+        squares[appleIndex].classList.add('apple');
     }
 
     // asign functions to keycodes
     function control(e) {
-        squares[currentIndex].classList.remove("snake"); //we are removing the class of snake
+        squares[currentIndex].classList.remove('snake'); //we are removing the class of snake
 
-        if (e.keyCode === 39) {
+        if (e.keyCode === 39) { // right arrow
             direction = 1; //if we press the right arrow on our keyboard, the snake will turn right.
         } else if (e.keyCode === 38) {
             direction = -width; // up arrow
         } else if (e.keyCode === 37) {
             direction = -1; // left arrow
-        } else if (e.keyCode === 40) {
+        } else if (e.keyCode === 40) { // down arrow
             direction = +width;
         }
+
     }
 
-    document.addEventListener("keyup", control);
-    startBtn.addEventListener("click", startGame);
-    restartBtn.addEventListener("click", startGame);
+    document.addEventListener('keyup', control);
+    startBtn.addEventListener('click', startGame);
+    restartBtn.addEventListener('click', startGame);
+
 });
